4c585e9c1be33bfc585d35fd7b5e5535bd8a38b0
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-24 21:42:10 -0500

    references added, has the references of the html

---
+++ src/js/references.js
@@ -0,0 +1,6 @@
+// References
+export const search         = document.querySelector( '#buscar' ),
+             form           = document.querySelector( '#formulario-buscar' ),
+             headingResult  = document.querySelector( '.letra-resultado h2' ),
+             result         = document.querySelector( '#resultado' ),
+             message        = document.querySelector( '#mensajes' );
